b36b29fb477a4eab3552bd0be3d79f07ad069228
MatijaKukec <44850103+MatijaKukec@users.noreply.github.com>
2018-12-12 13:45:05 +0100

    Create StatusKosarice.cs

---
+++ StatusKosarice.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using KosaricaZaKupovinu2;
+
+namespace KosaricaZaKupovinu2
+{
+
+    public enum Status
+    {
+          Prazna,
+          Aktivna,
+          Placena,
+          Stornirana
+    }
+}
